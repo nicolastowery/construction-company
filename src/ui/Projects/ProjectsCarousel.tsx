af085c30edd78ee93404cc36047c44ce0736be80
Nicolas Towery <nicolastowery@gmail.com>
2024-01-23 10:52:02 -0600

    Update ProjectsCarousel.tsx

--- src/ui/Projects/ProjectsCarousel.tsx
+++ src/ui/Projects/ProjectsCarousel.tsx
@@ -44,36 +44,4 @@ export default function ProjectsCarousel() {
       </div>
     </div>
   );
-  //   return (
-  //     <div className={styles.projectCardsComponentContainer}>
-  //       <div className={styles.projectCardsContainer}>
-  //         {projects.map((p) => (
-  //           <ProjectCard
-  //             imgFilePath={p.imgFilePath}
-  //             title={p.title}
-  //             address={p.address}
-  //           />
-  //         ))}
-  //       </div>
-  //       <div className={styles.sliderControlsContainer}>
-  //         <button className={styles.sliderBackButton}>
-  //           <img src="icons/left-arrow.svg" />
-  //           <span>Back</span>
-  //         </button>
-  //         <div className={styles.sliderIconContainer}>
-  //           <div
-  //             className={`${styles.sliderIconFilled} ${styles.sliderIcon}`}
-  //           ></div>
-  //           <div className={styles.sliderIcon}></div>
-  //           <div className={styles.sliderIcon}></div>
-  //           <div className={styles.sliderIcon}></div>
-  //           <div className={styles.sliderIcon}></div>
-  //         </div>
-  //         <button className={styles.sliderNextButton}>
-  //           <span>Next</span>
-  //           <img src="icons/right-arrow.svg" />
-  //         </button>
-  //       </div>
-  //     </div>
-  //   );
 }
